d7a0d2a591bc88534f5318a6b6946d1544486ce1
Matheus Coutinho <matheuscavalcante88@hotmail.com>
2020-06-05 11:39:02 -0300

    backend validation

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import path from 'path';
 import cors from 'cors';
+import { errors } from 'celebrate';
 import routes from './routes';
 
 const app = express();
@@ -11,4 +12,5 @@ app.use(routes);
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
+app.use(errors());
 app.listen(3333);
